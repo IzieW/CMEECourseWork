be624fd863d643d96ab04c13248895ce05a0ab84
Izie Wood <iw121@ic.ac.uk>
2021-10-25 14:04:03 +0100

    added example script

---
+++ Week2/code/MyExampleScript.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env Python3 
+"""Example script to run"""
+__author__ = 'Izie Wood (iw121@ic.ac.uk)'
+
+## functions ##
+def foo(x):
+    """Print x squared"""
+    x *= x # same as x = x*x
+    print(x)
+
+foo(2)
